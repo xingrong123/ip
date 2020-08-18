89fcec69effcb8841dd7f6920b573bf94e4f7cb2
xingrong123 <teyxingrong@gmail.com>
2020-08-18 22:24:45 +0800

    Add Increment: Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,16 +3,30 @@ import java.util.Scanner;
 public class Duke {
 
     private static final String LINE = "\t____________________________________________________________" + System.lineSeparator();
+    private static String[] list = new String[100];
+    private static int listSize = 0;
 
-    public static void echo(String input) {
-        System.out.print(LINE + "\t" + input + System.lineSeparator() + LINE);
+    public static void printList() {
+        System.out.print(LINE);
+        for(int i = 0; i < listSize; i++) {
+            System.out.printf("\t%d. %s\n", i + 1, list[i]);
+        }
+        System.out.println(LINE);
+    }
+
+    public static void addList(String input) {
+        System.out.println(LINE +
+                "\tadded: "+ input + System.lineSeparator() +
+                LINE);
+        list[listSize] = input;
+        listSize++;
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = "";
 
-        System.out.print(LINE +
+        System.out.println(LINE +
                 "\tHello! I'm Duke\n" +
                 "\tWhat can I do for you?\n" +
                 LINE);
@@ -21,7 +35,10 @@ public class Duke {
             input = scanner.nextLine();
             if(input.toLowerCase().equals("bye"))
                 break;
-            echo(input);
+            else if(input.toLowerCase().equals("list"))
+                printList();
+            else if(listSize < 100)
+                addList(input);
         }
 
 
