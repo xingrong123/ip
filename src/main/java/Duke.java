7a87a7df0404d14eb33f1168323dfa28a5ed5538
xingrong123 <teyxingrong@gmail.com>
2020-08-18 21:44:22 +0800

    Add Increment: Level 1. Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,32 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private static final String LINE = "\t____________________________________________________________" + System.lineSeparator();
+
+    public static void echo(String input) {
+        System.out.print(LINE + "\t" + input + System.lineSeparator() + LINE);
+    }
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________" + System.lineSeparator();
-        System.out.print(line +
-                        " Hello! I'm Duke\n " +
-                        " What can I do for you?\n" +
-                        line +
-                        " Bye. Hope to see you again soon!\n" +
-                        line);
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+
+        System.out.print(LINE +
+                "\tHello! I'm Duke\n" +
+                "\tWhat can I do for you?\n" +
+                LINE);
+
+        while(true) {
+            input = scanner.nextLine();
+            if(input.toLowerCase().equals("bye"))
+                break;
+            echo(input);
+        }
+
+
+        System.out.println(LINE +
+                "\tBye. Hope to see you again soon!\n" +
+                LINE);
     }
 }
