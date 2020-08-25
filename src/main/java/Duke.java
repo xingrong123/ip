b121ac828ccd1ee41345d1616d121f936f12453a
xingrong123 <teyxingrong@gmail.com>
2020-08-25 20:01:41 +0800

    Update code to comply with coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,26 +2,27 @@ import java.util.Scanner;
 
 public class Duke {
 
-    private static final String LINE = "\t____________________________________________________________" + System.lineSeparator();
+    private static final String LINE = "\t____________________________________________________________"
+            + System.lineSeparator();
     private static TaskList taskList;
 
     public static void printList() {
-        System.out.print(LINE +
-                "\tHere are the tasks in your list:\n");
+        System.out.print(LINE
+                + "\tHere are the tasks in your list:\n");
         taskList.printTaskList();
         System.out.println(LINE);
     }
 
     public static void addList(String input) {
-        System.out.println(LINE +
-                "\tadded: "+ input + System.lineSeparator() +
-                LINE);
+        System.out.println(LINE
+                + "\tadded: "+ input + System.lineSeparator()
+                + LINE);
         taskList.addTask(input);
     }
 
     public static void markDone(int listOrder) {
-        System.out.println(LINE +
-                "\tNice! I've marked this task as done:");
+        System.out.println(LINE
+                + "\tNice! I've marked this task as done:");
         taskList.markTaskDone(listOrder);
         System.out.println(LINE);
     }
@@ -40,38 +41,44 @@ public class Duke {
         String input = "";
         taskList = new TaskList();
 
-        System.out.println(LINE +
-                "\tHello! I'm Duke\n" +
-                "\tWhat can I do for you?\n" +
-                LINE);
+        System.out.println(LINE
+                + "\tHello! I'm Duke\n"
+                + "\tWhat can I do for you?\n"
+                + LINE);
 
         while(true) {
             input = scanner.nextLine();
-            if (input.trim().toLowerCase().equals("bye"))
+            if (input.trim().toLowerCase().equals("bye")) {
                 break;
-            else if (input.trim().toLowerCase().equals("list"))
+            } else if (input.trim().toLowerCase().equals("list")) {
                 printList();
-            else if (input.trim().toLowerCase().startsWith("done ")) {
+            } else if (input.trim().toLowerCase().startsWith("done ")) {
                 String inputCheck = input.trim().substring(5).trim();
+
                 if (isInteger(inputCheck)) {
                     int number = Integer.parseInt(inputCheck);
-                    if (number <= taskList.getTaskListSize() && number > 0)
+                    // Checks if number after done is valid
+                    // inputs not recognized as valid instructions are added as tasks
+                    if (number <= taskList.getTaskListSize() && number > 0) {
                         markDone(Integer.parseInt(inputCheck));
-                    else
+                    } else {
                         addList(input);
-                }
-                else
+                    }
+
+                } else {
                     addList(input);
-            }
-            else if (taskList.getTaskListSize() < 100 && !input.isBlank())
+                }
+
+            } else if (taskList.getTaskListSize() < 100 && !input.isBlank()) {
                 addList(input);
-            else if(!input.isBlank())
+            } else if(!input.isBlank()) {
                 System.out.println("\tlist is full");
+            }
         }
 
 
-        System.out.println(LINE +
-                "\tBye. Hope to see you again soon!\n" +
-                LINE);
+        System.out.println(LINE
+                + "\tBye. Hope to see you again soon!\n"
+                + LINE);
     }
 }
